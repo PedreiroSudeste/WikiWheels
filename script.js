3c1f1d0eab9cb7f27b7446b7735b3868c701f1fd
PedreiroSudeste <75530913+PedreiroSudeste@users.noreply.github.com>
2022-04-28 23:09:25 -0300

    enter

---
+++ script.js
@@ -0,0 +1,46 @@
+function avaliar1(){
+    if (document.querySelector(".btnAvaliar1").style.backgroundColor == "") {
+        document.querySelector('.btnAvaliar1').style.backgroundColor = "rgb(212 175 55)";
+        document.querySelector('.btnAvaliar1').style.color = "black";
+    }else{
+        document.querySelector('.btnAvaliar1').style.backgroundColor = "";
+        document.querySelector('.btnAvaliar1').style.color = "white";
+    }
+}
+function avaliar2(){
+    if (document.querySelector(".btnAvaliar2").style.backgroundColor == "") {
+        document.querySelector('.btnAvaliar2').style.backgroundColor = "rgb(212 175 55)";
+        document.querySelector('.btnAvaliar2').style.color = "black";
+    }else{
+        document.querySelector('.btnAvaliar2').style.backgroundColor = "";
+        document.querySelector('.btnAvaliar2').style.color = "white";
+    }
+    
+}
+function avaliar3(){
+    if (document.querySelector(".btnAvaliar3").style.backgroundColor == "") {
+        document.querySelector('.btnAvaliar3').style.backgroundColor = "rgb(212 175 55)";
+        document.querySelector('.btnAvaliar3').style.color = "black";
+    }else{
+        document.querySelector('.btnAvaliar3').style.backgroundColor = "";
+        document.querySelector('.btnAvaliar3').style.color = "white";
+    }
+}
+function avaliar4(){
+    if (document.querySelector(".btnAvaliar4").style.backgroundColor == "") {
+        document.querySelector('.btnAvaliar4').style.backgroundColor = "rgb(212 175 55)";
+        document.querySelector('.btnAvaliar4').style.color = "black";
+    }else{
+        document.querySelector('.btnAvaliar4').style.backgroundColor = "";
+        document.querySelector('.btnAvaliar4').style.color = "white";
+    }
+}
+function avaliar5(){
+    if (document.querySelector(".btnAvaliar5").style.backgroundColor == "") {
+        document.querySelector('.btnAvaliar5').style.backgroundColor = "rgb(212 175 55)";
+        document.querySelector('.btnAvaliar5').style.color = "black";
+    }else{
+        document.querySelector('.btnAvaliar5').style.backgroundColor = "";
+        document.querySelector('.btnAvaliar5').style.color = "white";
+    }
+}
